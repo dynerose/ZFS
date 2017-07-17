d38c97e62dba3ea829f908c8e6d373b67836745a
dynerose <dynerose@gmail.com>
2017-07-17 08:39:38 +0200

    initialize2

--- postinstall.sh
+++ postinstall.sh
@@ -10,18 +10,68 @@
 # TODO: --LUKS encrypt zfs
 # TODO: --mdm raid boot
 
+#----- Import Functions -----#
+
+dir="$(dirname "$0")"
+
+. $dir/functions/utilities
+
+# Prompt Colors
+BGREEN='\[\033[1;32m\]'
+GREEN='\[\033[0;32m\]'
+BRED='\[\033[1;31m\]'
+RED='\[\033[0;31m\]'
+BBLUE='\[\033[1;34m\]'
+BLUE='\[\033[0;34m\]'
+NORMAL='\[\033[00m\]'
+PS1="${BLUE}(${NORMAL}\w${BLUE}) ${NORMAL}\u${BLUE}@\h${RED}\$ ${NORMAL}"
 
 # Constants
 readonly VERSION="0.1 Alpha"
 readonly TRUE=0
 readonly FALSE=1
-
 readonly REQ_PKGS="debootstrap gdisk zfs zfs-initramfs"
-readonly ZFSPOOL="rpool"
-readonly ZFSMNTPOINT="/mnt"
-readonly ZFS_CRYPT="rpool_crypt"
+export readonly ZFSPOOL="rpool"
+export readonly ZFSMNTPOINT="/mnt"
+export readonly ZFS_CRYPT="rpool_crypt"
 # end / Constants
 
 # Initialize / Variable
+VAR_UEFI=$FALSE
+VAR_LUKS=$FALSE
+VAR_MDM=$FALSE
+VAR_VERBOSE=0
+
+VAR_USERNAME=username
+VAR_PASSWORD=password
+
+VAR_MINDISK=2
+
 
 # end / Variable
+
+#----- Fancy Messages -----#
+show_error(){
+echo -e "\033[1;31m$@\033[m" 1>&2
+033[1;31m
+}
+show_info(){
+echo -e "\033[1;32m$@\033[0m"
+}
+show_warning(){
+echo -e "\033[1;33m$@\033[0m"
+}
+show_question(){
+echo -e "\033[1;34m$@\033[0m"
+}
+show_success(){
+echo -e "\033[1;35m$@\033[0m"
+}
+show_header(){
+echo -e "\033[1;36m$@\033[0m"
+}
+show_listitem(){
+echo -e "\033[0;37m$@\033[0m"
+}
+
+unmount_all

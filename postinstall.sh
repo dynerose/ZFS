95ae6b997df93fd2c7a6560b572b558328f4a7ac
dynerose <dynerose@gmail.com>
2017-07-17 08:06:48 +0200

    initialize2

---
+++ postinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# postinstall.sh
+# see postinstall.sh -h for info/help
+
+# TOTO: --zfs zfs on root
+# TODO: --bootstrap option(s) to install minimal bootable environment as alternative to --cp or --install opts
+# TODO: --uefi untested/unfinished
+# TODO: --swap to support creation of a swap volume on pool
+# TODO: seperate core vs optional sets (rpool/home vs rpool/srv)
+# TODO: --LUKS encrypt zfs
+# TODO: --mdm raid boot
+
+
